7e2745fe8297ea792481bf18be120bed714923b3
KirTarasko <kir.tarasko@gmail.com>
2022-06-07 19:36:29 +0200

    Initial Commit

---
+++ WebBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebBrowser
+//
+//  Created by Kirill Tarasko on 07.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
